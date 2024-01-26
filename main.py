9db88b89dd7bc4741c45f38505bc5b7923a04de1
Nicholas Noochla-or <nnlaor@gmail.com>
2024-01-25 23:20:40 -0500

    adding more structure to the application

---
+++ main.py
@@ -0,0 +1,19 @@
+from helpers.network_utilities import calculate_icmp_checksum, ping
+
+
+class server:
+    def __init__(self) -> None:
+        pass
+
+
+class menu:
+    def __init__(self) -> None:
+        self.server_list = []
+        self.current_menu = None
+    
+    
+
+
+
+if __name__ == "__main__":
+    pass
